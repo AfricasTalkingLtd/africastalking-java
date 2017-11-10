b7e37cb5e674c1816c0d9f4776366ed6321d8437
Salama Balekage <aksalj@aksalj.com>
2017-11-10 21:46:22 +0100

    Clean up fake keys

--- libs/core/src/test/java/com/africastalking/test/Fixtures.java
+++ libs/core/src/test/java/com/africastalking/test/Fixtures.java
@@ -1,8 +1,8 @@
 package com.africastalking.test;
 
 public final class Fixtures {
-    public static final String API_KEY = "a8153533b29af2dc32b5458e340ef815523d026c7b684febe17e22b23e694312";
-    public static final String USERNAME = "sandbox";
+    public static final String API_KEY = "fake";
+    public static final String USERNAME = "fake";
     public static final boolean DEBUG = true;
     public static final long TIMEOUT = 3500;
 }
