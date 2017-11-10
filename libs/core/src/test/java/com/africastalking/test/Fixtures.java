f027053a8be269a8cd71250c632a28992e0699bd
Salama Balekage <aksalj@aksalj.com>
2017-11-10 21:42:40 +0100

    Clean up build

--- libs/core/src/test/java/com/africastalking/test/Fixtures.java
+++ libs/core/src/test/java/com/africastalking/test/Fixtures.java
@@ -1,10 +1,8 @@
 package com.africastalking.test;
 
-import com.africastalking.BuildConfig;
-
 public final class Fixtures {
-    public static final String API_KEY = BuildConfig.API_KEY;
-    public static final String USERNAME = BuildConfig.USERNAME;
+    public static final String API_KEY = "a8153533b29af2dc32b5458e340ef815523d026c7b684febe17e22b23e694312";
+    public static final String USERNAME = "sandbox";
     public static final boolean DEBUG = true;
     public static final long TIMEOUT = 3500;
 }
